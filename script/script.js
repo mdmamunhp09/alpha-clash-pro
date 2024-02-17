b874134ac00619263dc590ae02d9ec20bf262ede
mdmamunhp09 <mdmamunhp09@gmail.com>
2024-02-17 15:08:38 +0600

    play agin funciton create and manufulate

--- script/script.js
+++ script/script.js
@@ -3,9 +3,10 @@ function play() {
     showElementById('play-ground');
     hideElementById('final-score');
     setElementTextById('life-area',5);
-
+    setElementTextById('score-area',0)
     continueGame()
 }
+
 
 
 
@@ -18,26 +19,38 @@ function handleKeyboardButtonPress(event) {
         //update score
         const updateScore=currentScore+1;
        setElementTextById('score-area',updateScore); 
+       removeBackgroundById(randomKey)
+       continueGame();
     }
     else{
         const currentLife=getScoreAndLifeById('life-area');
         // updateLife
         const updateLife=currentLife-1;
-        setElementTextById('life-area',updateLife)
+        setElementTextById('life-area',updateLife);
 
         if(updateLife===0){
-            hideElementById('play-ground')
-            showElementById('final-score')
+            gameOver();
         }
     }
     
-    continueGame();
-    removeBackgroundById(randomKey)
+  
 }
 document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
 
 function continueGame() {
     const Alphabet = getRandomAlphabet();
     setElementTextById('screen-result', Alphabet);
     setBackgroundById(Alphabet);
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    const score=getScoreAndLifeById('score-area');
+    setElementTextById('score-final',score);
+    const currentAlphabet=getElementTextById('screen-result');
+    removeBackgroundById(currentAlphabet);
+    console.log(currentAlphabet);
 }
