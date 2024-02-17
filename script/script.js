c3127f11c4b053428bbd259ef8c9a7e552a3e6e7
mdmamunhp09 <mdmamunhp09@gmail.com>
2024-02-17 13:54:22 +0600

    generate alpahbet set screen random alphabet

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
-function play(){
+function play() {
     hideElementById('home-section');
     showElementById('play-ground')
+    continueGame()
 }
 
+function handleKeyboardButtonPress(event) {
+    const keyPress = event.key;
+
+}
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+function continueGame() {
+    const Alphabet = getRandomAlphabet();
+    setElementTextById('screen-result', Alphabet);
+}
--- script/utility.js
+++ script/utility.js
@@ -1,9 +1,34 @@
-function hideElementById(elementId){
-    const elementById=document.getElementById(elementId);
+function hideElementById(elementId) {
+    const elementById = document.getElementById(elementId);
     elementById.classList.add('hidden')
 }
 
-function showElementById(elementId){
-    const elementById=document.getElementById(elementId);
+function showElementById(elementId) {
+    const elementById = document.getElementById(elementId);
     elementById.classList.remove('hidden')
 }
+
+function getElementTextById(elementId) {
+    const ElementId = document.getElementById(elementId);
+    const ElementText =ElementId.innerText.toLocaleLowerCase();
+    return ElementText;
+}
+
+
+
+function setElementTextById(elementId, value) {
+    const ElementId = document.getElementById(elementId);
+    ElementId.innerText = value;
+}
+
+
+//random alphabet
+
+function getRandomAlphabet() {
+    const alphabetStr = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetArray = alphabetStr.split('');
+    const randomNumber = Math.round(Math.random() * 25);
+    const alphabet = alphabetArray[randomNumber];
+    return alphabet;
+}
+
