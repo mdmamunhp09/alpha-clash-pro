bf2e1d1e4e6514eaceed099e3b9d41751a5949c9
mdmamunhp09 <mdmamunhp09@gmail.com>
2024-02-21 03:51:02 +0600

    added modal and error screen color and success and error sound

--- script/script.js
+++ script/script.js
@@ -1,38 +1,40 @@
-function play() {
-    hideElementById('home-section');
-    showElementById('play-ground');
-    hideElementById('final-score');
-    setElementTextById('life-area',5);
-    setElementTextById('score-area',0)
-    continueGame()
-}
+let isAudio=false;
+const audio=new Audio();
 
-
-
+const artBoard=document.getElementById('artboard');
+const modalBox=document.getElementById('modal-box');
 
 function handleKeyboardButtonPress(event) {
+    if(isAudio==false) return;
     const keyPress = event.key;
     const randomKey=getElementTextById('screen-result');
 
     if(keyPress==="Escape"){
         hideElementById('play-ground');
-        showElementById('home-section')
+        showElementById('home-section');
+        removeBackgroundById(randomKey)
     }
     if(keyPress===randomKey){
         const currentScore=getScoreAndLifeById('score-area')
-       
+       audio.src="../audio/success.mp3";
+       audio.play();
         //update score
         const updateScore=currentScore+1;
        setElementTextById('score-area',updateScore); 
        removeBackgroundById(randomKey)
        continueGame();
+       
     }
     else{
         const currentLife=getScoreAndLifeById('life-area');
         // updateLife
         const updateLife=currentLife-1;
-        setElementTextById('life-area',updateLife);
+        const updateLifeColor=updateLife/5*100;
+        artBoard.style.background=`linear-gradient(#FFFFFFB3 ${updateLifeColor}% ,#ff000066)`
 
+        setElementTextById('life-area',updateLife);
+        audio.src="../audio/beep-03.mp3";
+        audio.play();
         if(updateLife===0){
             gameOver();
         }
@@ -50,6 +52,16 @@ function continueGame() {
     setBackgroundById(Alphabet);
 }
 
+function playGame() {
+    hideElementById('home-section');
+    showElementById('play-ground');
+    hideElementById('final-score');
+    setElementTextById('life-area',5);
+    setElementTextById('score-area',0)
+    continueGame()
+    isAudio=true;
+}
+
 function gameOver(){
     hideElementById('play-ground');
     showElementById('final-score');
@@ -57,5 +69,20 @@ function gameOver(){
     setElementTextById('score-final',score);
     const currentAlphabet=getElementTextById('screen-result');
     removeBackgroundById(currentAlphabet);
+    isAudio=false;
+    artBoard.style.background=`linear-gradient(#FFFFFFB3 100%,#ff000066)`
   
 }
+
+function modalOpen(event){
+   console.log(event)
+   if(event.clientY<20){
+    modalBox.style.display="flex";
+   }
+}
+
+function modalClose(){
+    modalBox.style.display="none"
+}
+
+document.body.onmousemove=modalOpen
